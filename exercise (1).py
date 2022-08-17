a4b43d15c199d815ba5cdd5d3e1a042204031da8
PascoPrzyborowski <unity01@gmx.net>
2022-08-17 11:09:26 +0200

    ex4

--- exercise (1).py
+++ exercise (1).py
@@ -5,49 +5,49 @@
 
 
 #Task 1
-print()
-print("Task1")
-print()
+# print()
+# print("Task1")
+# print()
 
-for i in range (2):
-    print("Give me two Numbers to compare:")
-    print()
-    num1= int(input("Give me  number 1 :"))
-    print()
-    num2= int(input("Give me  number 2 :"))
-    #print(num1, num2)
-    print("Your first Number is:",num1)
-    print("Your second Number is:",num2)
-    if num1 == num2:
-        print("Numbers are equal!")
-    elif num1 != num2:
-        print("Numbers are not equal!")
-    if num1 > num2:
-        print("Fist number is greater than the second Number!")
-    elif num1 < num2:
-        print("Fist number is smaller than the second Number!")
-    if num2 > num1:
-        print("Second number is greater than the first Number!")
-    elif num2 < num1:
-        print("Second number is smaller than the first Number!")
-    if num1 >= num2:
-        print("Fist number is greater than or equal to the second Number!")
-    elif num1 <= num2:
-        print("Fist number is smaller than or equal to the second Number!")
-    if num2 >= num1:
-        print("Second number is greater than or equal to the first Number!")
-    elif num2 <= num1:
-        print("Second number is smaller than or equal to the first Number!")
-    if num1 > 100 and num2 > 100:
-        print("Both numbers are big!")
-    elif num1 < 100 and num2 < 100:
-        print("Both numbers are small")
-    if num1 > 100 and num2 > 100:
-        big_num = True
-        print("The big_num is set to: ", big_num)
-    elif num1 < 100 and num2 < 100:
-        big_num = False
-        print("The big_num is set to: ", big_num)
+# for i in range (2):
+#     print("Give me two Numbers to compare:")
+#     print()
+#     num1= int(input("Give me  number 1 :"))
+#     print()
+#     num2= int(input("Give me  number 2 :"))
+#     #print(num1, num2)
+#     print("Your first Number is:",num1)
+#     print("Your second Number is:",num2)
+#     if num1 == num2:
+#         print("Numbers are equal!")
+#     elif num1 != num2:
+#         print("Numbers are not equal!")
+#     if num1 > num2:
+#         print("Fist number is greater than the second Number!")
+#     elif num1 < num2:
+#         print("Fist number is smaller than the second Number!")
+#     if num2 > num1:
+#         print("Second number is greater than the first Number!")
+#     elif num2 < num1:
+#         print("Second number is smaller than the first Number!")
+#     if num1 >= num2:
+#         print("Fist number is greater than or equal to the second Number!")
+#     elif num1 <= num2:
+#         print("Fist number is smaller than or equal to the second Number!")
+#     if num2 >= num1:
+#         print("Second number is greater than or equal to the first Number!")
+#     elif num2 <= num1:
+#         print("Second number is smaller than or equal to the first Number!")
+#     if num1 > 100 and num2 > 100:
+#         print("Both numbers are big!")
+#     elif num1 < 100 and num2 < 100:
+#         print("Both numbers are small")
+#     if num1 > 100 and num2 > 100:
+#         big_num = True
+#         print("The big_num is set to: ", big_num)
+#     elif num1 < 100 and num2 < 100:
+#         big_num = False
+#         print("The big_num is set to: ", big_num)
 
 
     
